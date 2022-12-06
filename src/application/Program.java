0a864ed257de38921a1aa3c5de1b29e7c37eb034
Vinícius Oliveira <vini-oliveira08@Hotmail.com>
2022-12-06 20:58:46 -0300

    Adicionado pacote entidade e aplicação

---
+++ src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import model.entities.Cliente;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Cliente obj = new Cliente(1,"Pedro", "Rueda");
+		
+		System.out.println(obj);
+		
+	}
+	
+	
+	
+}
---
+++ src/model/entities/Cliente.java
@@ -0,0 +1,79 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Cliente implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String nome;
+	private String sobrenome;
+
+	
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+
+
+	
+	public Cliente() {
+		
+	}
+	
+	public Cliente(int id, String nome, String sobrenome) {
+	
+		this.id = id;
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nome=" + nome + "]";
+	}
+	
+
+}
